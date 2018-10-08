7ae0c13c82849199423114634a3b859de02eaca8
Mohnish G J <mail4mohnish@gmail.com>
2018-10-09 00:32:18 +0530

    Mix format test file

--- test/binance/order_book_depth_stream_test.exs
+++ test/binance/order_book_depth_stream_test.exs
@@ -7,7 +7,7 @@ defmodule Binance.OrderBookDepthStreamTest do
       order_book_info_type = "bids"
 
       assert Binance.OrderBookDepthStream.price_list(order_book, order_book_info_type) ==
-        [6667.16, 6666.0]
+               [6667.16, 6666.0]
     end
 
     test "It extracts a list of prices related to the asks order book info type" do
@@ -15,7 +15,7 @@ defmodule Binance.OrderBookDepthStreamTest do
       order_book_info_type = "asks"
 
       assert Binance.OrderBookDepthStream.price_list(order_book, order_book_info_type) ==
-        [6668.98, 6669.18, 6669.23, 6822.0]
+               [6668.98, 6669.18, 6669.23, 6822.0]
     end
 
     test "It raises an error for an incorrectly specified order book info type" do
@@ -30,8 +30,8 @@ defmodule Binance.OrderBookDepthStreamTest do
 
   def order_book_depth_stream_info do
     %Binance.OrderBookDepthStream{
-      E: 1539017941520,
-      U: 260960815,
+      E: 1_539_017_941_520,
+      U: 260_960_815,
       a: [
         ["6668.98000000", "0.05793100", []],
         ["6669.18000000", "0.00000000", []],
@@ -41,7 +41,7 @@ defmodule Binance.OrderBookDepthStreamTest do
       b: [["6667.16000000", "4.78806400", []], ["6666.00000000", "0.13463200", []]],
       e: "depthUpdate",
       s: "BTCUSDT",
-      u: 260960822
+      u: 260_960_822
     }
   end
 end
